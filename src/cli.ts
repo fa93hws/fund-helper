23924a1b743b140acf2bcdf1ac74a015831f8842
Eric Wang <wjun0912@gmail.com>
2020-02-29 11:32:26 +1100

    Add yargs command (#1)

--- src/cli.ts
+++ src/cli.ts
@@ -1,14 +1,12 @@
-import { calculateBasics } from './analyze/analyze';
-import { HttpService } from './services/http/http';
-import { EastMoneyService } from './services/eastmoney/eastmoney';
+import * as yargs from 'yargs';
+import { addStatisticsCommand } from './commands/statistics';
 
 export async function main() {
-  const httpService = new HttpService();
-  const eastMoneyService = new EastMoneyService(httpService);
-
-  const { netValues } = await eastMoneyService.getNetValues({
-    id: '160220',
-    pageNum: 1,
-  });
-  console.log(calculateBasics(netValues));
+  addStatisticsCommand(yargs)
+    .strict(true)
+    .exitProcess(true)
+    .demandCommand()
+    .showHelpOnFail(false, 'Specify --help for available options')
+    .help()
+    .parse();
 }
