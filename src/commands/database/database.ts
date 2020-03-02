50c1ce098b0403aa0b717725d26a33fc1c38a91d
eric wang <eric@canva.com>
2020-03-03 00:45:47 +1100

    show help on empty command

--- src/commands/database/database.ts
+++ src/commands/database/database.ts
@@ -1,4 +1,5 @@
-import { Argv } from 'yargs';
+import { Argv, showHelp } from 'yargs';
+import { red } from 'chalk';
 import { createPool } from '../../services/database/connection';
 import { PGService } from '../../services/database/pgservice';
 
@@ -11,14 +12,15 @@ async function initHandler() {
 }
 
 function defaultHandler() {
-  console.error('you must provide a sub command, type --help for help');
+  console.error(red('you must provide a sub command'));
+  showHelp();
 }
 
 export function addDatabaseCommand(yargs: Argv) {
   return yargs.command('database', 'Manipulate databases', {
-    builder: (yargsIn): Argv<{}> =>
-      yargsIn.command('init', 'Purge database and recreate everything.', {
-        builder: y => y,
+    builder: (y): Argv<{}> =>
+      y.command('init', 'Purge database and recreate everything.', {
+        builder: yy => yy,
         handler: initHandler,
       }),
     handler: defaultHandler,
