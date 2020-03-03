90da7698250b9bc86b55fc9e0a7482f84f4839e2
Eric Wang <wjun0912@gmail.com>
2020-03-03 21:11:51 +1100

    Allow download fund info list to database (#18)

--- src/services/database/init.sql
+++ src/services/database/init.sql
@@ -1,14 +1,18 @@
-DROP TABLE IF EXISTS fund_info;
 DROP TABLE IF EXISTS fund_net_values;
+DROP TABLE IF EXISTS fund_info;
 
 CREATE TABLE fund_info (
   id varchar(10) PRIMARY KEY,
   name varchar(64) NOT NULL,
   type varchar(64) NOT NULL
 );
+CREATE INDEX fund_info_name_idx on fund_info(name);
+CREATE INDEX fund_info_type_idx on fund_info(type);
 
-CREATE TABLE fund_net_values(
-  id varchar(10) PRIMARY KEY,
+CREATE TABLE fund_net_values (
+  id varchar(10) REFERENCES fund_info(id),
   time TIMESTAMP NOT NULL,
-  value float(4) NOT NULL
+  value float(4) NOT NULL,
+  PRIMARY KEY (id, time)
 );
+CREATE INDEX fund_net_values_value_idx on fund_net_values(value);
