717a5f9125dd688b4c2104c38b9b73635f19d41b
eric wang <eric@canva.com>
2020-03-03 23:39:55 +1100

    Change statistics command to positional arguments

--- src/commands/statistics/tests/statistics-out-template.tests.ts
+++ src/commands/statistics/tests/statistics-out-template.tests.ts
@@ -5,6 +5,7 @@ describe('formatOutput', () => {
     const parameters = {
       fundId: '123456',
       fundName: '某某基金',
+      fundType: '某某类型',
       average: 2.435,
       max: 2.543,
       min: 2.345,
@@ -13,9 +14,11 @@ describe('formatOutput', () => {
     expect(formatOutput(parameters)).toMatchInlineSnapshot(`
       "基金ID: 123456
       基金名称: 某某基金
+      基金类型: 某某类型
       123日均值: 2.435
       123日最高: 2.543
-      123日最低: 2.345"
+      123日最低: 2.345
+      "
     `);
   });
 });
