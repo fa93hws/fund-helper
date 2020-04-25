53c8add6beeb78c2f9346c9a1b615f1501413177
Eric Wang <wjun0912@gmail.com>
2020-04-25 14:07:07 +1000

    test: Add unit test to east money service (#30)

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,5 +1,8 @@
 mod http_service;
-pub use http_service::HttpService;
+pub use http_service::{create_unknown_error, HttpError};
+pub use http_service::{HttpService, IHttpService};
 
 mod east_money_service;
 pub use east_money_service::EastMoneyService;
+
+pub use http_service::MockHttpService;
