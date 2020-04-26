88cbc21d575e7903676cb9d25de71f405648eca5
Eric Wang <wjun0912@gmail.com>
2020-04-26 20:25:59 +1000

    feat: Add fund type to record  (#38)

--- src/services/east_money/fund_list_model.rs
+++ src/services/east_money/fund_list_model.rs
@@ -6,9 +6,44 @@ use crate::utils::context::FetchListContext;
 use serde_json::Value;
 
 #[derive(Debug, PartialEq)]
+pub enum FundType {
+    Mix,
+    Bond,
+    Index,
+    QDII,
+    Stock,
+    NotInterested,
+}
+
+#[derive(Debug, PartialEq)]
 pub struct FundListItem {
     id: String,
     name: String,
+    typ: FundType,
+}
+
+fn normalize_fund_type(
+    typ: &String,
+    context: &FetchListContext,
+) -> Result<FundType, Box<dyn DeserializationError>> {
+    match &typ[..] {
+        "混合型" | "混合-FOF" => Ok(FundType::Mix),
+        "债券型" | "定开债券" | "债券指数" => Ok(FundType::Bond),
+        "联接基金" | "股票指数" | "QDII-指数" | "ETF-场内" | "QDII-ETF" => {
+            Ok(FundType::Index)
+        }
+        "QDII" => Ok(FundType::QDII),
+        "股票型" | "其他创新" | "股票-FOF" => Ok(FundType::Stock),
+        "货币型" | "固定收益" | "理财型" | "分级杠杆" | "保本型" => {
+            Ok(FundType::NotInterested)
+        }
+        _ => Err(Box::new(TypeMismatchError {
+            expected_type: String::from("fund type enum"),
+            got: String::from(typ),
+            field: String::from("cells[3]"),
+            context: format!("{}", context),
+        })),
+    }
 }
 
 pub type FundList = Vec<FundListItem>;
@@ -54,7 +89,13 @@ pub fn extract_fund_list(
         }
         let id = deserialize_str(&tup[0], "0(fund-id)", context)?;
         let name = deserialize_str(&tup[2], "2(fund-name)", context)?;
-        list.push(FundListItem { id, name });
+        let typ = deserialize_str(&tup[3], "3(fund-type)", context)?;
+        let typ_enum = normalize_fund_type(&typ, context)?;
+        list.push(FundListItem {
+            id,
+            name,
+            typ: typ_enum,
+        });
     }
 
     Ok(list)
@@ -72,10 +113,12 @@ mod test {
             FundListItem {
                 id: String::from("000001"),
                 name: String::from("华夏成长混合"),
+                typ: FundType::Mix,
             },
             FundListItem {
                 id: String::from("000002"),
                 name: String::from("华夏成长混合(后端)"),
+                typ: FundType::Mix,
             },
         ];
         match extract_fund_list(&String::from(raw_response), &CONTEXT) {
@@ -108,6 +151,66 @@ mod test {
         match extract_fund_list(&String::from(raw_response), &CONTEXT) {
             Ok(_) => panic!("It should not be parsed correctly"),
             Err(e) => assert!(e.is_prefix_error()),
+        }
+    }
+
+    #[test]
+    fn test_normalize_fund_type_mixed() {
+        let typ = String::from("混合型");
+        let typ_enum_result = normalize_fund_type(&typ, &CONTEXT);
+        match typ_enum_result {
+            Ok(typ_enum) => assert_eq!(typ_enum, FundType::Mix),
+            Err(_) => panic!("It should pass"),
+        }
+    }
+
+    #[test]
+    fn test_normalize_fund_type_bond() {
+        let typ = String::from("债券型");
+        let typ_enum_result = normalize_fund_type(&typ, &CONTEXT);
+        match typ_enum_result {
+            Ok(typ_enum) => assert_eq!(typ_enum, FundType::Bond),
+            Err(_) => panic!("It should pass"),
+        }
+    }
+
+    #[test]
+    fn test_normalize_fund_type_index() {
+        let typ = String::from("股票指数");
+        let typ_enum_result = normalize_fund_type(&typ, &CONTEXT);
+        match typ_enum_result {
+            Ok(typ_enum) => assert_eq!(typ_enum, FundType::Index),
+            Err(_) => panic!("It should pass"),
+        }
+    }
+
+    #[test]
+    fn test_normalize_fund_type_stock() {
+        let typ = String::from("股票型");
+        let typ_enum_result = normalize_fund_type(&typ, &CONTEXT);
+        match typ_enum_result {
+            Ok(typ_enum) => assert_eq!(typ_enum, FundType::Stock),
+            Err(_) => panic!("It should pass"),
+        }
+    }
+
+    #[test]
+    fn test_normalize_fund_type_others() {
+        let typ = String::from("理财型");
+        let typ_enum_result = normalize_fund_type(&typ, &CONTEXT);
+        match typ_enum_result {
+            Ok(typ_enum) => assert_eq!(typ_enum, FundType::NotInterested),
+            Err(_) => panic!("It should pass"),
+        }
+    }
+
+    #[test]
+    fn test_normalize_fund_type_error() {
+        let typ = String::from("顾茗");
+        let typ_enum_result = normalize_fund_type(&typ, &CONTEXT);
+        match typ_enum_result {
+            Ok(_) => panic!("It should fail"),
+            Err(e) => assert!(e.is_type_error()),
         }
     }
 }
