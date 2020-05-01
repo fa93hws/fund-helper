310cc96547668c5527c6203f200eb91ce0e7c6f8
eric wang <eric@canva.com>
2020-05-01 13:07:33 +1000

    .

--- src/commands/server.rs
+++ src/commands/server.rs
@@ -1,5 +1,5 @@
+use actix_web::{get, web, App, HttpServer, Responder};
 use tokio::signal;
-use actix_web::{web, App, HttpServer, Responder, get};
 
 #[get("/api_v1/{id}")]
 async fn index(info: web::Path<u32>) -> impl Responder {
@@ -8,15 +8,20 @@ async fn index(info: web::Path<u32>) -> impl Responder {
 
 pub async fn main() {
     let local = tokio::task::LocalSet::new();
-    local.run_until(async move {
-        tokio::task::spawn_local(async move {
-            let local = tokio::task::LocalSet::new();
-            let sys = actix_rt::System::run_in_tokio("server", &local);
-            HttpServer::new(|| {
-                App::new().service(index)
-            }).bind("127.0.0.1:8080").unwrap().run();
-            signal::ctrl_c().await.expect("failed to listen for event");
-            println!("received ctrl-c event");
-        }).await.unwrap();
-    }).await;
+    local
+        .run_until(async move {
+            tokio::task::spawn_local(async move {
+                let local = tokio::task::LocalSet::new();
+                let sys = actix_rt::System::run_in_tokio("server", &local);
+                HttpServer::new(|| App::new().service(index))
+                    .bind("127.0.0.1:8080")
+                    .unwrap()
+                    .run();
+                signal::ctrl_c().await.expect("failed to listen for event");
+                println!("received ctrl-c event");
+            })
+            .await
+            .unwrap();
+        })
+        .await;
 }
