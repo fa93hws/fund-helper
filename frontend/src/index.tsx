879dccc15c1049056b7e3892152d355f74afc3c5
eric wang <eric@canva.com>
2020-05-09 12:29:03 +1000

    new: Add dummy frontend page

---
+++ frontend/src/index.tsx
@@ -0,0 +1,8 @@
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+
+export const App = () => (
+  <div>hello world</div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
