82383174773b6513bd091c844e2d7883d7b719b4
Eric Wang <wjun0912@gmail.com>
2020-05-29 00:34:13 +1000

    feat: Allow fetch and update fund info and fundlist from database (#79)

--- backend-ts/src/modules/cn-fund/tests/values-service.tests.ts
+++ backend-ts/src/modules/cn-fund/tests/values-service.tests.ts
@@ -12,19 +12,95 @@ describe('valuesService', () => {
     getValues: fakeEastMoneyGetValues,
   } as any) as EastMoneyService;
 
-  const pgService = ({} as any) as PGService;
+  const fakePgInsert = jest.fn();
+  const fakePgSelect = jest.fn();
+  const pgService = ({
+    insert: fakePgInsert,
+    select: fakePgSelect,
+  } as any) as PGService;
   const fakeService = new FundValueService(fakeEastMoneyService, pgService);
 
   beforeEach(() => {
     fakeEastMoneyGetValues.mockRestore();
     fakeEastMoneyGetList.mockRestore();
+    fakePgInsert.mockRestore();
+    fakePgSelect.mockRestore();
   });
 
   describe('getList', () => {
-    it('forward to east money service', async () => {
-      fakeEastMoneyGetList.mockReturnValueOnce(of());
-      await fakeService.getList();
-      expect(fakeEastMoneyGetList).toBeCalledTimes(1);
+    it('update db with result from east money service', async (done) => {
+      fakeEastMoneyGetList.mockReturnValueOnce(
+        of({
+          kind: 'ok',
+          data: [
+            { id: '123', name: 'fund-name', type: 'A' },
+            { id: '12', name: 'fund', type: 'B' },
+          ],
+        }),
+      );
+      fakePgInsert.mockReturnValue({ kind: 'ok' });
+      const result = await fakeService.getList();
+      if (result.kind === 'ok') {
+        expect(result.data).toEqual([
+          { id: '123', name: 'fund-name', type: 'A' },
+          { id: '12', name: 'fund', type: 'B' },
+        ]);
+        expect(fakePgInsert).toBeCalledWith(
+          {
+            fields: ['id', 'name', 'type'],
+            tableName: 'fund_info',
+            values: [
+              ['$1', '$2', '$3'],
+              ['$4', '$5', '$6'],
+            ],
+            conflict: {
+              field: 'id',
+              set: [
+                {
+                  field: 'name',
+                  value: 'EXCLUDED.name',
+                },
+                {
+                  field: 'type',
+                  value: 'EXCLUDED.type',
+                },
+              ],
+            },
+          },
+          ['123', 'fund-name', 'A', '12', 'fund', 'B'],
+        );
+        done();
+      } else {
+        done.fail('it should not fail');
+      }
+    });
+    it('returns error if request fails', async () => {
+      fakeEastMoneyGetList.mockReturnValueOnce(
+        of({
+          kind: 'error',
+          error: 'error!',
+        }),
+      );
+      const result = await fakeService.getList();
+      expect(result).toEqual({
+        kind: 'error',
+        error: 'error!',
+      });
+      expect(fakePgInsert).not.toBeCalled();
+    });
+    it('returns error if update fails', async () => {
+      fakeEastMoneyGetList.mockReturnValueOnce(
+        of({
+          kind: 'ok',
+          data: [{ id: '123', name: 'fund-name', type: 'A' }],
+        }),
+      );
+      fakePgInsert.mockReturnValue({ kind: 'error', error: 'error!' });
+      const result = await fakeService.getList();
+      expect(result).toEqual({
+        kind: 'error',
+        error: 'error!',
+      });
     });
   });
 
@@ -120,6 +196,90 @@ describe('valuesService', () => {
         kind: 'error',
         error: 'err',
       });
+    });
+  });
+
+  describe('getFundInfo', () => {
+    it('return info directly without send request if it is in the DB', async () => {
+      fakePgSelect.mockReturnValue({
+        kind: 'ok',
+        data: {
+          rowCount: 1,
+          rows: [{ id: '123', name: 'fund-name', type: 'type' }],
+        },
+      });
+      const result = await fakeService.getFundInfo('123');
+      expect(result).toEqual({
+        kind: 'ok',
+        data: { id: '123', name: 'fund-name', type: 'type' },
+      });
+      expect(fakePgSelect).toBeCalledWith(
+        {
+          fields: ['id', 'name', 'type'],
+          tableName: 'fund_info',
+          where: 'id = $1',
+        },
+        ['123'],
+      );
+    });
+    it('sends the request and update DB if not found in the db at the first time', async () => {
+      fakePgSelect.mockReturnValueOnce({
+        kind: 'ok',
+        data: {
+          rowCount: 0,
+          rows: [],
+        },
+      });
+      fakePgSelect.mockReturnValueOnce({
+        kind: 'ok',
+        data: {
+          rowCount: 1,
+          rows: [{ id: '123', name: 'fund-name', type: 'type' }],
+        },
+      });
+      fakePgInsert.mockReturnValue({ kind: 'ok' });
+      fakeEastMoneyGetList.mockReturnValue(
+        of({
+          kind: 'ok',
+          data: [],
+        }),
+      );
+      const result = await fakeService.getFundInfo('123');
+      expect(result).toEqual({
+        kind: 'ok',
+        data: { id: '123', name: 'fund-name', type: 'type' },
+      });
+      expect(fakePgSelect).toBeCalledTimes(2);
+      expect(fakeEastMoneyGetList).toBeCalled();
+      expect(fakePgInsert).toBeCalled();
+    });
+    it('returns error if not found in second trial', async (done) => {
+      fakePgSelect.mockReturnValue({
+        kind: 'ok',
+        data: {
+          rowCount: 0,
+          rows: [],
+        },
+      });
+      fakePgInsert.mockReturnValue({ kind: 'ok' });
+      fakeEastMoneyGetList.mockReturnValue(
+        of({
+          kind: 'ok',
+          data: [],
+        }),
+      );
+      const result = await fakeService.getFundInfo('123');
+      if (result.kind === 'error') {
+        expect(fakePgSelect).toBeCalledTimes(2);
+        expect(fakeEastMoneyGetList).toBeCalled();
+        expect(fakePgInsert).toBeCalled();
+        expect(result.error).toMatchInlineSnapshot(
+          `[Error: 0 or multiple results found for fundId=123]`,
+        );
+        done();
+      } else {
+        done.fail('it should fail');
+      }
     });
   });
 });
--- backend-ts/src/services/database/pg.service.ts
+++ backend-ts/src/services/database/pg.service.ts
@@ -9,9 +9,24 @@ export type PGServiceParam = {
   port: number;
 };
 
-type SelectStatement = {
+type BaseStatement = {
   fields: string[];
   tableName: string;
+};
+
+export type SelectStatement = BaseStatement & {
+  where?: string;
+};
+
+type InsertStatement = BaseStatement & {
+  conflict?: {
+    field: string;
+    set?: {
+      field: string;
+      value: string;
+    }[];
+  };
+  values: string[][];
 };
 
 @Injectable()
@@ -32,15 +47,57 @@ export class PGService {
 
   async select<T>(
     statement: SelectStatement,
+    params?: any[],
   ): Promise<Result.T<{ rows: T[]; rowCount: number }, any>> {
-    const { fields, tableName } = statement;
-    const sql = `SELECT ${fields.join(',')} FROM ${tableName}`;
+    const { fields, tableName, where } = statement;
+    const sqlBuffer = [`SELECT ${fields.join(',')} FROM ${tableName}`];
+    if (where != null) {
+      sqlBuffer.push(`WHERE ${where}`);
+    }
     try {
-      const queryResult = await this.pool.query<T>(sql);
+      const queryResult = await this.pool.query<T>(sqlBuffer.join(' '), params);
       return Result.createOk({
         rows: queryResult.rows,
         rowCount: queryResult.rowCount,
       });
+    } catch (e) {
+      return Result.createError(e);
+    }
+  }
+
+  async insert(
+    statement: InsertStatement,
+    params?: any[],
+  ): Promise<Result.T<number, any>> {
+    const { fields, tableName, conflict, values } = statement;
+    const sqlBuffer = [`INSERT INTO ${tableName} (${fields.join(',')}) VALUES`];
+    const valueBuffer = [];
+    for (const value of values) {
+      if (value.length !== fields.length) {
+        return Result.createError(
+          new Error(
+            `got values length ${value.length} but fields length ${fields.length}. values: ${value}, fields: ${fields}`,
+          ),
+        );
+      }
+      valueBuffer.push(`(${value.join(',')})`);
+    }
+    sqlBuffer.push(valueBuffer.join(','));
+    if (conflict != null) {
+      sqlBuffer.push(`ON CONFLICT (${conflict.field}) DO`);
+      const { set } = conflict;
+      if (set == null) {
+        sqlBuffer.push('NOTHING');
+      } else {
+        const setStatements = set.map(
+          ({ field, value }) => `${field} = ${value}`,
+        );
+        sqlBuffer.push('UPDATE SET', setStatements.join(','));
+      }
+    }
+    try {
+      const queryResult = await this.pool.query(sqlBuffer.join(' '), params);
+      return Result.createOk(queryResult.rowCount);
     } catch (e) {
       return Result.createError(e);
     }
