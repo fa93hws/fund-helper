dbb23988d843a2b0f9c7dabbc61504fa120b797a
eric wang <eric@canva.com>
2020-05-30 12:55:55 +1000

    check unused

--- backend-ts/src/modules/database/pg.module.ts
+++ backend-ts/src/modules/database/pg.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '../config/config.module';
 import { PGService } from './pg.service';
-import { PGConfigProvider } from '../config/pg-config.provider';
-// import { pgServiceFactory } from './pg.factory';
 
 @Module({
   imports: [ConfigModule],
