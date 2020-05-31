83ec3664db175dc4d0cf085cac37cb408c3a02a0
eric wang <eric@canva.com>
2020-05-31 12:04:02 +1000

    chore: separate protos definition

--- backend/src/protos/fund-cn.proto.ts
+++ backend/src/protos/fund-cn.proto.ts
@@ -1,23 +1,6 @@
-export type FundValueCN = {
-  time: Date;
-  value: number;
-};
-
-export const enum FundTypeCN {
-  '混合' = '混合',
-  '股票' = '股票',
-  '债券' = '债券',
-  '指数' = '指数',
-  '其他' = '其他',
-}
-
-export type FundBasicInfoCN = {
-  id: string;
-  name: string;
-  type: FundTypeCN;
-};
-
-export type FundValueWithInfoCN = {
-  values: FundValueCN[];
-  info: FundBasicInfoCN;
-};
+export {
+  FundValueCN,
+  FundTypeCN,
+  FundBasicInfoCN,
+  FundValueWithInfoCN,
+} from '../../../protos/fund-cn.proto';
