07fb987cb9a781a9f0df407253992f352a8604ac
eric wang <eric@canva.com>
2020-06-02 13:46:30 +1000

    fix tests

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,11 +1,10 @@
+const baseConfig = require('@fund-helper/eslint-config');
+
 module.exports = {
+  ...baseConfig,
   root: true,
-  parser: '@typescript-eslint/parser',
   env: {
     node: true,
-  },
-  parserOptions: {
-    sourceType: 'module',
   },
   settings: {
     'import/resolver': {
@@ -13,33 +12,5 @@ module.exports = {
         extensions: ['.ts'],
       },
     },
-  },
-  plugins: ['@typescript-eslint', 'babel', 'jest'],
-  extends: [
-    'airbnb-base',
-    'eslint-config-prettier',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-  ],
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-namespace': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-
-    'import/extensions': 'off',
-    'import/no-default-export': 'error',
-    'import/prefer-default-export': 'off',
-
-    'jest/no-test-callback': 'off',
-
-    'no-useless-constructor': 'off',
-    'no-unused-expressions': 'off',
-    'no-console': 'error',
-    'class-methods-use-this': 'off',
-    'no-continue': 'off',
-    'max-classes-per-file': 'off',
-    'no-restricted-syntax': 'off',
   },
 };
